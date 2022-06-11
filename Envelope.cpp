2944d486b1315c8a833358e5946c6694e3c07b26
Skulllady <noorhoque45@gmail.com>
2022-06-11 08:52:20 -0700

    added envelope logic

--- Envelope.cpp
+++ Envelope.cpp
@@ -28,17 +28,41 @@ void Envelope::setL2(float value)
 {
   l2 = value;
 }
+// value ms
+void Envelope::setT3(int value)
+{
+  t3 = value;
+}
+
+// value between 0.0 and 1.0
+void Envelope::setL3(float value)
+{
+  l3 = value;
+}
+
+void Envelope::setT4(int value)
+{
+  t4 = value;
+}
 
 // returns level betwee 0.0 and 1.0 given time in ms
 float Envelope::getLevel(int t)
 {
   float l0 = 0.0;
+  float l4 = 0.0;
   int t0 = 0;
   if (t <= t1) {
     return l0 + (l1 - l0) * (float) (t - t0) / t1;
   } else if (t < t1 + t2) {
-      return l1 + (l2 - l1) * (float) (t - t1) / t2;
+      return l1 + (l2 - l1) * (float) (t - t1 - t0) / t2;
+  }else if (t < t1 + t2 +t3) {
+      return l2 + (l3 - l2) * (float) (t - t2 - t1 - t0) / t3;
   } else {
-    return l2;
+    float level = l3 + (l4 - l3) * (float) (t - t3 - t2 - t1 - t0) / t4;
+    if (level > 0.0) {
+      return level;
+    }else{
+      return 0.0;
+    }
   }
   }
--- Envelope.h
+++ Envelope.h
@@ -9,14 +9,20 @@ class Envelope
     Envelope();
     void setT1(int value); //ms
     void setL1(float value);
-        void setT2(int value); //ms
+    void setT2(int value); //ms
     void setL2(float value);
+    void setT3(int value); //ms
+    void setL3(float value);
+    void setT4(int value); //release
     float getLevel(int time); //returns 0.0 - 1.0
   private:
     int t1; // ms
     int t2; // ms
+    int t3; // ms
+    int t4; // release
     float l1; // 0.0 - 1.0
     float l2;
+    float l3;
 };
 
 #endif
